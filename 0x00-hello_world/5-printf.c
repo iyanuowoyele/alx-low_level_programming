a0f29e36ba3169c85644b71c4b4e01cbbdd95ac8
iyanuowoyele <owoyeleiyanuoluwapo@gmail.com>
2022-06-02 16:23:46 -0700

    cool

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,13 @@
-
 #include <stdio.h>
 
 /**
+ * main - main block
  *
- *    * main - Prints onto screen
- *
- *       *
- *
- *          * Return: zero
- *
- *             */
+ * Return: 0
+ */
 
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
