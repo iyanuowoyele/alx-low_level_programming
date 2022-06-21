d907e2df0544fc574613f77629f81162c5742ff2
iyanuowoyele <owoyeleiyanuoluwapo@gmail.com>
2022-06-20 22:13:18 -0700

    cool

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,33 +12,21 @@ unsigned int _strspn(char *s, char *accept)
 {
 
 int i, j;
-
 int count = 0;
-
 char *str1, *str2;
 
 str1 = s;
-
 str2 = accept;
 
 i = 0;
-
 while (str1[i] != '\0') /*Declaring WHILE *s */
-
 {
-
 j = 0;
-
 while (str2[j] != '\0') /*Declaring WHILE *accept*/
-
 {
-
 if (str2[j] == str1[i]) /*Evaluate condition*/
-
 {
-
 count++; /*count number*/
-
 break;
 
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 /**
-* print_diagsums - the sum of the two diagonals of a square matrix of integers
-* @a: 2d array of chars
+ * print_diagsums - the sum of the two diagonals of a square matrix of integers
+ * @a: 2d array of chars
 *@size: number matrix
-*Return: void
-*/
+ *Return: void
+ */
 
 void print_diagsums(int *a, int size)
 
@@ -31,9 +31,9 @@ while (i < size) /*number repetitions*/
 
 /*sums for diagsums*/
 
-sum1 = sum1 + *(a + i * size + i);
+sum1 = sum1 + *(a + i *size + i);
 
-sum2 = sum2 + *(a + i * size + size - i - 1);
+sum2 = sum2 + *(a + i *size + size - i - 1);
 
 i++; /*add +1*/
 
